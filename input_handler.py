0d2dc2d03b4f0585da70da7b42d66ec454660512
Cubball <138378823+Cubball@users.noreply.github.com>
2024-02-06 20:14:24 +0200

    Add an input handler class

---
+++ input_handler.py
@@ -0,0 +1,8 @@
+from minefield import Minefield
+
+class InputHandler:
+    def __init__(self, minefield: Minefield):
+        self.minefield = minefield
+
+    def handle_input(self, event, minefield_rec):
+        pass
