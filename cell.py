7cf84aeb2ed560053ae34ccace2116762b3e1cd6
Cubball <138378823+Cubball@users.noreply.github.com>
2024-02-06 17:26:35 +0200

    Add a cell class

--- cell.py
+++ cell.py
@@ -4,3 +4,24 @@ class CellState(Enum):
     CLOSED = 0
     OPEN = 1
     FLAGGED = 2
+
+class Cell:
+    def __init__(self, hasBomb):
+        self.hasBomb = hasBomb
+        self.state = CellState.CLOSED
+
+    def try_open(self) -> bool:
+        if self.state == CellState.FLAGGED:
+            return False
+
+        self.state = CellState.OPEN
+        return False
+
+    def toggle_flag(self):
+        if self.state == CellState.OPEN:
+            return
+        
+        if self.state == CellState.CLOSED:
+            self.state = CellState.FLAGGED
+        else:
+            self.state = CellState.CLOSED
