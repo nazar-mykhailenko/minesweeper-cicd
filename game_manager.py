427969da387e2b9a47d73fc235f857082fbe9917
Cubball <138378823+Cubball@users.noreply.github.com>
2024-02-07 19:49:36 +0200

    Handle clicks for menu buttons, except the stats button

--- game_manager.py
+++ game_manager.py
@@ -22,10 +22,12 @@ class GameManager:
 
         # HACK: find a better way to retrieve the Rects?
         self.clickable_elements = {
+            self.on_minefield_click: self.renderer.get_field_coords(),
             self.on_menu_button_click: self.renderer.get_menu_button_coords(),
+            self.on_exit_button_click: self.renderer.get_exit_button_coords(),
+            self.on_resume_button_click: self.renderer.get_resume_button_coords(),
             self.on_restart_button_click: self.renderer.get_restart_button_coords(),
             self.on_stats_button_click: (0, 0, 0, 0),
-            self.on_minefield_click: self.renderer.get_field_coords(),
         }
 
     def run_game(self):
@@ -43,10 +45,13 @@ class GameManager:
             if self.game_state == GameState.IN_PROGRESS:
                 seconds_elapsed = (pygame.time.get_ticks() - self.start_time) // 1000
 
-            if not self.menu_open:
-                self.renderer.draw(seconds_elapsed)
+            if self.menu_open:
+                if self.stats_open:
+                    self.renderer.draw_stats()
+                else:
+                    self.renderer.draw_menu()
             else:
-                self.renderer.draw_menu()
+                self.renderer.draw(seconds_elapsed)
 
             self.game_state = self.minefield.get_game_state()
 
@@ -90,6 +95,22 @@ class GameManager:
         if event.button == LEFT_MOUSE_BUTTON and self.menu_open:
             self.restart_game()
             self.menu_open = False
+            return True
+
+        return False
+
+
+    def on_resume_button_click(self, event, _):
+        if event.button == LEFT_MOUSE_BUTTON and self.menu_open:
+            self.menu_open = False
+            return True
+
+        return False
+
+
+    def on_exit_button_click(self, event, _):
+        if event.button == LEFT_MOUSE_BUTTON and self.menu_open:
+            self.running = False
             return True
 
         return False
