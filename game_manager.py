4ae3ec5606a61c13d9968d34a2249e2e5b8264ba
Cubball <138378823+Cubball@users.noreply.github.com>
2024-02-07 14:30:41 +0200

    Add event handlers in the game manager class

--- game_manager.py
+++ game_manager.py
@@ -1,10 +1,22 @@
 import pygame
+from record_manager import RecordManager
 
 FPS = 60
 
 class GameManager:
-    def __init__(self):
-        pass
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.record_manager = RecordManager()
+        self.menu_open = False
+        self.renderer = None
+        self.clickable_elements = []
+        # Empty tuples shold be replaced with Rects for each respective element
+        # Rects can be retrieved from the renderer which is None for now
+        self.clickable_elements.append(((), self.on_menu_button_click))
+        self.clickable_elements.append(((), self.on_restart_button_click))
+        self.clickable_elements.append(((), self.on_stats_button_click))
+        self.clickable_elements.append(((), self.on_minefield_click))
+
 
     def run_game(self):
         pass
@@ -17,7 +29,7 @@ class GameManager:
     def toggle_menu(self):
         pass
 
-    
+
     # Not sure if this belongs here
     def show_stats(self):
         pass
@@ -28,4 +40,20 @@ class GameManager:
 
 
     def handle_click(self):
+        pass
+
+
+    def on_menu_button_click(self, event, mouse_pos):
+        pass
+
+
+    def on_restart_button_click(self, event, mouse_pos):
+        pass
+
+
+    def on_stats_button_click(self, event, mouse_pos):
+        pass
+
+
+    def on_minefield_click(self, event, mouse_pos):
         pass
