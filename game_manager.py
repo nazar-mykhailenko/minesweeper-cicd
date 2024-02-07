915c57428255a4f3e08c349652884aa2bded4d52
Cubball <138378823+Cubball@users.noreply.github.com>
2024-02-07 14:19:37 +0200

    Add the Game Manager class

---
+++ game_manager.py
@@ -0,0 +1,31 @@
+import pygame
+
+FPS = 60
+
+class GameManager:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+
+
+    def restart_game(self):
+        pass
+
+
+    def toggle_menu(self):
+        pass
+
+    
+    # Not sure if this belongs here
+    def show_stats(self):
+        pass
+
+
+    def click_cell(self):
+        pass
+
+
+    def handle_click(self):
+        pass
