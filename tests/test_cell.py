26ae9841bb64b77b0849c8dbc81cefd0e6b23ca5
Cubball <138378823+Cubball@users.noreply.github.com>
2024-02-15 11:12:52 +0200

    Add the rest of the tests for cell class

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -11,3 +11,49 @@ class TestCell:
         cell = Cell(False)
         # Assert
         assert cell.state == CellState.CLOSED
+
+    def test_cell_has_mine_is_correct(self):
+        # Arrange, Act
+        cell_with_mine = Cell(True)
+        cell_without_mine = Cell(False)
+        # Assert
+        assert cell_with_mine.has_mine == True
+        assert cell_without_mine.has_mine == False
+
+    def test_toggle_flag_changes_cell_state_when_cell_is_closed(self):
+        # Arrange
+        cell = Cell(False)
+        # Act
+        cell.toggle_flag()
+        # Assert
+        assert cell.state == CellState.FLAGGED
+        # Act
+        cell.toggle_flag()
+        # Assert
+        assert cell.state == CellState.CLOSED
+
+    def test_toggle_flag_doesnt_change_state_when_cell_is_open(self):
+        # Arrange
+        cell = Cell(False)
+        cell.try_open()
+        # Act
+        cell.toggle_flag()
+        # Assert
+        assert cell.state == CellState.OPEN
+
+    def test_try_open_cell_opens_cell_when_cell_isnt_flagged(self):
+        # Arrange
+        cell = Cell(False)
+        # Act
+        cell.try_open()
+        # Assert
+        assert cell.state == CellState.OPEN
+
+    def test_try_open_cell_doesnt_open_cell_when_cell_is_flagged(self):
+        # Arrange
+        cell = Cell(False)
+        cell.toggle_flag()
+        # Act
+        cell.try_open()
+        # Assert
+        assert cell.state == CellState.FLAGGED
