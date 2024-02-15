824d0836aa9a6e56bc12037d6f36f79d0ef221ca
Cubball <138378823+Cubball@users.noreply.github.com>
2024-02-15 11:36:33 +0200

    Add a shell script to run tests and linter

---
+++ pipeline.sh
@@ -0,0 +1,9 @@
+Green="\033[0;32m"
+Yellow="\033[0;33m"
+NC="\033[0m"
+echo -e "${Yellow}Starting tests...${NC}"
+pytest .
+echo -e "${Green}Tests finished${NC}"
+echo -e "${Yellow}Starting linting...${NC}"
+flake8 . -v
+echo -e "${Green}Linting finished${NC}"
